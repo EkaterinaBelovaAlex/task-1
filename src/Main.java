c4ed6eb36a539d46c860f9842dc12bbdd62aa0a6
Katya Belova <katya.belka.belka@gmail.com>
2023-02-04 22:17:16 +0300

    add class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 5156; // стоимость билета
-        int bonus = 20; // количество рублей для одной бонусной мили
-        int freeMiles = ticketPrice / bonus; // расчет количества бонусных миль
-
-        System.out.println("На ваш счет начислено:");
-        System.out.println((freeMiles) + " миля(ль)");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
